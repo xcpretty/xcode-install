4c12f8d074b26080be5345e49ac33aa20e6eeeb3
Piet Brauer <piet@nerdishbynature.com>
2015-04-14 13:10:57 +0200

    Add DELIVER_USER and DELIVER_PASSWORD to fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 $:.unshift((ROOT + 'lib').to_s)
 $:.unshift((ROOT + 'spec').to_s)
 
+ENV['DELIVER_USER'] = "xcode-install"
+ENV['DELIVER_PASSWORD'] = "12345password"
+
 require 'bundler/setup'
 require 'bacon'
 require 'mocha-on-bacon'
