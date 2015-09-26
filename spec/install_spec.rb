7124ad41459651f51a1b101fef78f839c30b0ca8
JP Simard <jp@jpsim.com>
2015-09-26 16:13:01 -0700

    add spec for --url

--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -16,6 +16,13 @@ module XcodeInstall
       Command::Install.run(['6.3'])
     end
 
+    it 'downloads and installs with custom HTTP URL' do
+      url = 'http://yolo.com/xcode.dmg'
+      Installer.any_instance.expects(:download).with('6.3', true, url).returns('/some/path')
+      Installer.any_instance.expects(:install_dmg).with('/some/path', '-6.3', true, true)
+      Command::Install.run(['6.3', "--url=#{url}"])
+    end
+
     it 'downloads and installs and does not switch if --no-switch given' do
       Installer.any_instance.expects(:download).with('6.3', true, nil).returns('/some/path')
       Installer.any_instance.expects(:install_dmg).with('/some/path', '-6.3', false, true)
