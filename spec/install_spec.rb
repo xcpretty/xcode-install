aac32f049dd0dcc64617379ac3ab3f86af83f592
JP Simard <jp@jpsim.com>
2015-09-26 16:09:48 -0700

    fix specs

--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -11,19 +11,19 @@ module XcodeInstall
     end
 
     it 'downloads and installs' do
-      Installer.any_instance.expects(:download).with('6.3', true).returns('/some/path')
+      Installer.any_instance.expects(:download).with('6.3', true, nil).returns('/some/path')
       Installer.any_instance.expects(:install_dmg).with('/some/path', '-6.3', true, true)
       Command::Install.run(['6.3'])
     end
 
     it 'downloads and installs and does not switch if --no-switch given' do
-      Installer.any_instance.expects(:download).with('6.3', true).returns('/some/path')
+      Installer.any_instance.expects(:download).with('6.3', true, nil).returns('/some/path')
       Installer.any_instance.expects(:install_dmg).with('/some/path', '-6.3', false, true)
       Command::Install.run(['6.3', '--no-switch'])
     end
 
     it 'downloads without progress if switch --no-progress is given' do
-      Installer.any_instance.expects(:download).with('6.3', false).returns('/some/path')
+      Installer.any_instance.expects(:download).with('6.3', false, nil).returns('/some/path')
       Installer.any_instance.expects(:install_dmg).with('/some/path', '-6.3', true, true)
       Command::Install.run(['6.3', '--no-progress'])
     end
