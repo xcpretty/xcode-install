c7170ea2867cd0647edec5b21fe8daf54fec3ac3
JP Simard <jp@jpsim.com>
2015-10-09 09:42:30 -0700

    enable cli_spec failure spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,10 +2,10 @@ require File.expand_path('../spec_helper', __FILE__)
 
 module XcodeInstall
   describe Command::InstallCLITools do
-    # it 'fails if tools are already installed' do
-    #   Command::InstallCLITools.any_instance.expects(:installed?).returns(true)
-    #   Command::InstallCLITools.run.should.raise(CLAide::InformativeError)
-    # end
+    it 'fails if tools are already installed' do
+      Command::InstallCLITools.any_instance.expects(:installed?).returns(true)
+      lambda { Command::InstallCLITools.run }.should.raise(SystemExit)
+    end
 
     it 'runs if tools are not installed' do
       Command::InstallCLITools.any_instance.expects(:installed?).returns(false)
