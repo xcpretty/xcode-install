1964f1029a70bab45feebe4fc3cc13392d70801a
JScott <jvscott@gmail.com>
2017-04-03 18:07:13 -0700

    Replace the infamous Spotlight with system_profiler
    This adds 2 seconds to each run of `installed` in my
    experience. It's not ideal but it's also not awful.
    It returns the same results as far as I can tell in my
    tests. The regex expression is also tied to how
    xcode-install renames the downloaded Xcode.app files
    (i.e. `xcode_path`).

--- lib/xcode/install.rb
+++ lib/xcode/install.rb
@@ -259,12 +259,8 @@ HELP
     end
 
     def installed
-      unless (`mdutil -s /` =~ /disabled/).nil?
-        $stderr.puts 'Please enable Spotlight indexing for /Applications.'
-        exit(1)
-      end
-
-      `mdfind "kMDItemCFBundleIdentifier == 'com.apple.dt.Xcode'" 2>/dev/null`.split("\n")
+      app_xml = `system_profiler -detailLevel full SPApplicationsDataType -xml`
+      app_xml.scan(/<string>(.*?Xcode[-\d\.]*app)<\/string>/).flatten
     end
 
     def parse_seedlist(seedlist)
