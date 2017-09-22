6740ef4160ae7c7cbba3e2accfcfa3f576bba924
Danielle Tomlinson <dani@tomlinson.io>
2017-09-22 13:53:36 +0200

    Remove --all, and make it the default behaviour

--- lib/xcode/install/list.rb
+++ lib/xcode/install/list.rb
@@ -4,23 +4,9 @@ module XcodeInstall
       self.command = 'list'
       self.summary = 'List Xcodes available for download.'
 
-      def self.options
-        [['--all', 'Show all available versions.']].concat(super)
-      end
-
-      def initialize(argv)
-        @all = argv.flag?('all', false)
-        super
-      end
-
       def run
         installer = XcodeInstall::Installer.new
-
-        if @all
-          puts installer.list
-        else
-          puts installer.list_current
-        end
+        puts installer.list
       end
     end
   end
