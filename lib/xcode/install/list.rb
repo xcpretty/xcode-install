c1e8f04e6a486175ae4a11ee42258823b9194ae2
Danielle Tomlinson <dan@tomlinson.io>
2017-09-22 14:01:48 +0200

    Re-add --all flag

--- lib/xcode/install/list.rb
+++ lib/xcode/install/list.rb
@@ -4,6 +4,10 @@ module XcodeInstall
       self.command = 'list'
       self.summary = 'List Xcodes available for download.'
 
+      def self.options
+        [['--all', 'Show all available versions. (Default, Deprecated)']].concat(super)
+      end
+
       def run
         installer = XcodeInstall::Installer.new
         puts installer.list
