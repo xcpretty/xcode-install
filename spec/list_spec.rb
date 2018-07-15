7cf9e33c63e051f7931145ac424c5ab40b27f6b9
Steve Ohashi <steve@junkpiano.me>
2018-07-16 01:08:58 +0900

    added an annotation to beta in list

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -4,7 +4,6 @@ module XcodeInstall
   describe Command::List do
     before do
       installer.stubs(:exists).returns(true)
-      installer.stubs(:installed_versions).returns([])
     end
 
     def installer
@@ -23,10 +22,35 @@ module XcodeInstall
       installer.stubs(:xcodes).returns(xcodes)
     end
 
+    def fake_installed_xcode(name)
+      xcode_path = "/Applications/Xcode-#{name}.app"
+      xcode_version = name
+      xcode_version << '.0' unless name.include? '.'
+
+      installed_xcode = InstalledXcode.new(xcode_path)
+      installed_xcode.stubs(:version).returns(xcode_version)
+      installed_xcode.stubs(:bundle_version).returns(Gem::Version.new(xcode_version))
+      installed_xcode
+    end
+
+    def fake_installed_xcodes(*names)
+      xcodes = names.map { |name| fake_installed_xcode(name) }
+      installer.stubs(:installed_versions).returns(xcodes)
+    end
+
     describe '#list' do
       it 'lists all versions' do
         fake_xcodes '1', '2.3', '2.3.1', '2.3.2', '3 some', '4 beta', '10 beta'
+        fake_installed_xcodes
         installer.list.should == "1\n2.3\n2.3.1\n2.3.2\n3 some\n4 beta\n10 beta"
+      end
+    end
+
+    describe '#list_annotated' do
+      it 'lists all versions with annotations' do
+        fake_xcodes '1', '2.3', '2.3.1', '2.3.2', '3 some', '4.3.1 for Lion', '9.4.1', '10 beta'
+        fake_installed_xcodes '2.3', '4.3.1', '10'
+        installer.list.should == "1\n2.3 (installed)\n2.3.1\n2.3.2\n3 some\n4.3.1 for Lion (installed)\n9.4.1\n10 beta (installed)"
       end
     end
   end
