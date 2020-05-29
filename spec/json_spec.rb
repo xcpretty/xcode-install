4242811c434b7612473191ae76ef1b74a5176a7d
Piet Brauer <piet.brauer@mhp.com>
2020-05-29 15:20:20 +0200

    Apply new sorting to tests

--- spec/json_spec.rb
+++ spec/json_spec.rb
@@ -6,9 +6,9 @@ module XcodeInstall
       fixture = Pathname.new('spec/fixtures/xcode.json').read
       xcode = Xcode.new(JSON.parse(fixture))
 
-      xcode.date_modified.should == 1_413_472_373_000
-      xcode.name.should == 'Command Line Tools (OS X 10.9) for Xcode - Xcode 6.1'
-      xcode.url.should == 'https://developer.apple.com/devcenter/download.action?path=/Developer_Tools/command_line_tools_os_x_10.9_for_xcode__xcode_6.1/command_line_tools_for_osx_10.9_for_xcode_6.1.dmg'
+      xcode.date_modified.should == 1_572_613_080
+      xcode.name.should == '9.3'
+      xcode.url.should == 'https://developer.apple.com/devcenter/download.action?path=/Developer_Tools/Xcode_9.3/Xcode_9.3.xip'
     end
 
     it 'can parse list of all Xcodes' do
@@ -19,7 +19,7 @@ module XcodeInstall
       installer.stubs(:installed_versions).returns([])
       installer.stubs(:xcodes).returns(seedlist)
 
-      installer.list.should == "6.1\n6.1.1\n6.2"
+      installer.list.should == "4.3 for Lion\n4.3.1 for Lion\n4.3.2 for Lion\n4.3.3 for Lion\n4.4.1\n4.5\n4.6.2\n4.6\n4.6.1\n4.6.3\n5.0.1\n5\n5.0.2\n5.1\n5.1.1\n6.0.1\n6.1\n6.1.1\n6.2\n6.3\n6.3.1\n6.3.2\n6.4\n7\n7.0.1\n7.1\n7.1.1\n7.2.1\n7.2\n7.3\n7.3.1\n8\n8.1\n8.2\n8.2.1\n8.3.2\n8.3.3\n8.3\n9\n9.0.1\n9.1\n9.2\n9.3\n9.3.1\n9.4\n9.4.1\n10\n10.1\n10.2.1\n10.2\n10.3\n11\n11.1\n11.2\n11.2.1\n11.3 beta\n11.3\n11.3.1\n11.4 beta\n11.4\n11.4 beta 3\n11.4 beta 2\n11.4.1\n11.5 beta 2\n11.5\n11.5 GM Seed\n11.5 beta"
     end
 
     it 'raises informative error when account is not registered as a developer' do
